adfadbf55e656bb32607de0b73051fb8eaeb36b2
Neeraj-Sharma01 <ns975821@gmail.com>
2024-02-06 08:52:34 +0530

    latestchanges

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
 
 const parent = React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{id:"head1"},"hello World"),React.createElement("h1",{id:"head2"},"hello World")]),React.createElement("div",{id:"child"},[React.createElement("h1",{id:"head1"},"hello World"),React.createElement("h1",{id:"head2"},"hello World")])]);
 console.log(parent);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #React Code
+
+#Parcel
+ dev build
+ local Server
+ HMR- Hot Module Replacement
+ file Watching alogrithm -Written in C++
+ Caching- Faster Build
